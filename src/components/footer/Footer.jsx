460a49a293bd5d8448ad9c6fed183e8fef86ecab
jessica <jessica.santosb@outlook.com>
2024-03-08 15:17:27 -0300

    Delete footer animation

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,4 @@
-import React, { useContext, useEffect, useRef } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useContext } from "react";
 import "./Footer.css";
 import { LanguageContext } from "../../contexts/LanguageContext";
 import ProfilesIcons from "../../utils/profile icons/ProfilesIcons";
@@ -8,58 +6,8 @@ import ProfilesIcons from "../../utils/profile icons/ProfilesIcons";
 function Footer() {
   const { languageData } = useContext(LanguageContext);
 
-  const footerRef = useRef(null);
-
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    gsap.fromTo(
-      ".footer-links",
-      { x: "-200px", opacity: 0 },
-      {
-        x: 0,
-        opacity: 1,
-        duration: 1.5,
-        ease: "power1.in",
-        scrollTrigger: {
-          trigger: footerRef.current,
-          start: "bottom 100",
-        },
-      }
-    );
-
-    gsap.fromTo(
-      ".footer-icons",
-      { x: "200px", opacity: 0 },
-      {
-        x: 0,
-        opacity: 1,
-        duration: 1.5,
-        ease: "power1.in",
-        scrollTrigger: {
-          trigger: footerRef.current,
-          start: "bottom 100",
-        },
-      }
-    );
-
-    gsap.fromTo(
-      ".footer-copyright",
-      { y: "200px" },
-      {
-        y: 0,
-        duration: 2,
-        ease: "power1.in",
-        scrollTrigger: {
-          trigger: footerRef.current,
-          start: "bottom",
-        },
-      }
-    );
-  }, []);
-
   return (
-    <footer id="footer" className="disable-selection" ref={footerRef}>
+    <footer id="footer" className="disable-selection">
       <ul className="footer-links">
         <li>
           <a className="footer-link" href="#hero">
