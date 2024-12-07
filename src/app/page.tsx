112c4f7243c73b88730777b9a44c5f22b136ec0b
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-07 16:53:30 -0300

    feat: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,24 @@ import { HomeTitle } from "@/components/title";
 
 export default function Home() {
   return (
-    <section className="w-full min-h-screen flex items-center justify-center">
-      <div className="p-4">
-        <div className="flex items-center flex-col lg:flex-row gap-4">
-          <HomeTitle text="freelance" />
-          <div className="bg-green-200 h-[7vw] w-full" />
-        </div>
+    <section className="w-full min-h-[80vh] pt-20 md:pt-8 lg:pt-20 px-4 md:px-8">
+      <div className="flex flex-col lg:flex-row gap-0 lg:gap-4">
+        <HomeTitle text="freelance" />
+        <div className="bg-green-200 h-[8vw] w-full" />
+      </div>
 
-        <div className="flex items-center flex-col-reverse lg:flex-row gap-4">
-          <div className="bg-blue-200 h-[7vw] w-full" />
-          <HomeTitle text="Dev Frontend" />
-        </div>
+      <div className="pt-4 lg:p-0 flex flex-col-reverse lg:flex-row gap-0 lg:gap-4">
+        <div className="bg-green-200 h-[8vw] w-full" />
+        <HomeTitle text="Dev Frontend" />
+      </div>
 
-        <div className="flex items-center flex-col lg:flex-row gap-4">
-          <HomeTitle text="Jéssica Bandeira" />
-          <p className="w-full lg:w-52 my-auto lg:text-xs tracking-wide leading-6">
-            Olá! Sou uma desenvolvedora Web com foco em Frontend, sempre
-            empenhada em expandir e refinar minhas habilidades.
-          </p>
-        </div>
+      <div className="pt-4 lg:p-0 flex items-center flex-col lg:flex-row gap-4">
+        <HomeTitle text="Jéssica Bandeira" />
+        <p className="w-full lg:w-52 my-auto lg:text-xs tracking-wide leading-6">
+          Olá! Sou uma desenvolvedora Web especializada em Frontend, dedicada em
+          unir arte e funcionalidades para criar experiências digitais
+          incríveis.
+        </p>
       </div>
     </section>
   );
