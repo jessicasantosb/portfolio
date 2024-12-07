da91dbed941a680f593cd933e78bdd029d004f7c
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-07 15:24:33 -0300

    feat: create flip link component

---
+++ src/components/flip-link.tsx
@@ -0,0 +1,75 @@
+import { motion } from "framer-motion";
+
+const DURATION = 0.25;
+const STAGGER = 0.025;
+
+type Props = {
+  children: string;
+  href: string;
+};
+
+export const FlipLink = ({ children, href }: Props) => {
+  return (
+    <motion.a
+      initial="initial"
+      whileHover="hovered"
+      href={href}
+      target="_blank"
+      className="relative block overflow-hidden whitespace-nowrap font-black uppercase text-[14vw]"
+      style={{
+        lineHeight: 0.8,
+      }}
+    >
+      {children && (
+        <>
+          <div>
+            {children.split("").map((l, i) => (
+              <motion.span
+                variants={{
+                  initial: {
+                    y: 0,
+                  },
+                  hovered: {
+                    y: "-100%",
+                  },
+                }}
+                transition={{
+                  duration: DURATION,
+                  ease: "easeInOut",
+                  delay: STAGGER * i,
+                }}
+                className="inline-block"
+                key={i}
+              >
+                {l}
+              </motion.span>
+            ))}
+          </div>
+          <div className="absolute inset-0">
+            {children.split("").map((l, i) => (
+              <motion.span
+                variants={{
+                  initial: {
+                    y: "100%",
+                  },
+                  hovered: {
+                    y: 0,
+                  },
+                }}
+                transition={{
+                  duration: DURATION,
+                  ease: "easeInOut",
+                  delay: STAGGER * i,
+                }}
+                className="inline-block"
+                key={i}
+              >
+                {l}
+              </motion.span>
+            ))}
+          </div>
+        </>
+      )}
+    </motion.a>
+  );
+};
