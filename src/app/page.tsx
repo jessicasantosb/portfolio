4ee168ae83529ac5090063dc6f06f61fa9ce9887
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-07 13:10:10 -0300

    feat: create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import { HomeTitle } from "@/components/title";
+
 export default function Home() {
-  return <section className="w-full min-h-screen bg-[#f6f6f6]"></section>;
+  return (
+    <section className="w-full min-h-screen flex items-center justify-center">
+      <div className="p-4">
+        <div className="flex items-center flex-col lg:flex-row gap-4">
+          <HomeTitle text="freelance" />
+          <div className="bg-green-200 h-[7vw] w-full" />
+        </div>
+
+        <div className="flex items-center flex-col-reverse lg:flex-row gap-4">
+          <div className="bg-blue-200 h-[7vw] w-full" />
+          <HomeTitle text="Dev Frontend" />
+        </div>
+
+        <div className="flex items-center flex-col lg:flex-row gap-4">
+          <HomeTitle text="Jéssica Bandeira" />
+          <p className="w-full lg:w-52 my-auto lg:text-xs tracking-wide leading-6">
+            Olá! Sou uma desenvolvedora Web com foco em Frontend, sempre
+            empenhada em expandir e refinar minhas habilidades.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
