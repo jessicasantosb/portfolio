2d75c018c4f11c685e7f4d6de20b3a8cbc49fe12
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-07 15:26:36 -0300

    feat: add flip links

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { FlipLink } from "./flip-link";
 
 export const RevealLinks = () => {
   return (
-    <section className="w-full h-full pt-20 p-4 grid place-content-center gap-2">
+    <section className="grid place-content-center gap-2">
       <FlipLink
         href={`https://wa.me/${process.env.NEXT_PUBLIC_WHATSAPP_NUMBER}`}
       >
@@ -37,79 +37,5 @@ export const RevealLinks = () => {
         X
       </FlipLink>
     </section>
-  );
-};
-
-const DURATION = 0.25;
-const STAGGER = 0.025;
-
-type Props = {
-  children: string;
-  href: string;
-};
-
-const FlipLink = ({ children, href }: Props) => {
-  return (
-    <motion.a
-      initial="initial"
-      whileHover="hovered"
-      href={href}
-      target="_blank"
-      className="relative block overflow-hidden whitespace-nowrap font-black uppercase text-[14vw]"
-      style={{
-        lineHeight: 0.8,
-      }}
-    >
-      {children && (
-        <>
-          <div>
-            {children.split("").map((l, i) => (
-              <motion.span
-                variants={{
-                  initial: {
-                    y: 0,
-                  },
-                  hovered: {
-                    y: "-100%",
-                  },
-                }}
-                transition={{
-                  duration: DURATION,
-                  ease: "easeInOut",
-                  delay: STAGGER * i,
-                }}
-                className="inline-block"
-                key={i}
-              >
-                {l}
-              </motion.span>
-            ))}
-          </div>
-          <div className="absolute inset-0">
-            {children.split("").map((l, i) => (
-              <motion.span
-                variants={{
-                  initial: {
-                    y: "100%",
-                  },
-                  hovered: {
-                    y: 0,
-                  },
-                }}
-                transition={{
-                  duration: DURATION,
-                  ease: "easeInOut",
-                  delay: STAGGER * i,
-                }}
-                className="inline-block"
-                key={i}
-              >
-                {l}
-              </motion.span>
-            ))}
-          </div>
-        </>
-      )}
-    </motion.a>
   );
 };
