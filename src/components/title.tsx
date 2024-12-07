c47ad434f5f6221295b2b19d8a7b3d1dbb1e8afd
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-07 16:25:26 -0300

    style: change letter spacing  on title

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,6 +1,6 @@
 export function HomeTitle({ text }: { text: string }) {
   return (
-    <h1 className="text-[13vw] lg:text-[10vw] self-start tracking-tighter leading-none text-nowrap">
+    <h1 className="text-[13vw] lg:text-[10vw] self-start font-black tracking-[-3.5px] md:tracking-[-7px] lg:tracking-[-10px] leading-none text-nowrap">
       {text}
     </h1>
   );
