33bb6072132a7a2ce156346331e9a7da135e084d
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-07 13:09:31 -0300

    style: update title style

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,6 +1,6 @@
 export function HomeTitle({ text }: { text: string }) {
   return (
-    <h1 className="text-[10vw] self-start tracking-widest lg:tracking-tighter leading-none text-nowrap">
+    <h1 className="text-[13vw] lg:text-[10vw] self-start tracking-tighter leading-none text-nowrap">
       {text}
     </h1>
   );
