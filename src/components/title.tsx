6fcacf1ec8dbe076dad675c14fc29708f98ac656
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-08 20:02:43 -0300

    style: decrease line height on title

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,6 +1,6 @@
 export function HomeTitle({ text }: { text: string }) {
   return (
-    <h1 className="text-[9vh] md:text-[9vw] uppercase font-black tracking-[-7px] md:text-nowrap leading-tight sm:leading-normal ">
+    <h1 className="text-[9vh] md:text-[9vw] uppercase font-black tracking-[-7px] md:text-nowrap leading-none sm:leading-normal ">
       {text}
     </h1>
   );
