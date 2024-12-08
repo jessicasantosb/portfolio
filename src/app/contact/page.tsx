9e413e7f02bed4986de062ad402fb8b10ad53232
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-08 19:59:03 -0300

    style: increase links text height

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import { RevealLinks } from "@/components/links";
 
 export default function Contact() {
   return (
-    <section className="w-full min-h-[80vh] pt-20 px-4">
+    <section className="w-full min-h-[80vh] py-20 px-4">
       <RevealLinks />
     </section>
   );
