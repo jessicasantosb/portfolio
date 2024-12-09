b659211810389f5e96d13c4ed81baea5c18585da
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-09 18:56:17 -0300

    style: decrease home title font size

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,6 +1,6 @@
 export function HomeTitle({ text }: { text: string }) {
   return (
-    <h1 className="text-[8vh] md:text-[9vw] uppercase tracking-[-7px] md:tracking-[-13px] md:text-nowrap leading-none sm:leading-normal ">
+    <h1 className="text-[8vh] md:text-[7vw] uppercase tracking-[-7px] md:text-nowrap leading-none sm:leading-normal ">
       {text}
     </h1>
   );
