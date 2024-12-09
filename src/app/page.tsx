a1af108079954a7a83ef4061a57d969e604a5174
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-09 19:31:29 -0300

    style: handle home overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { HomeTitle } from "@/components/title";
 
 export default function Home() {
   return (
-    <section className="max-w-6xl h-full mx-auto py-16 px-4 md:px-8 bg-transparent">
+    <section className="max-w-6xl h-full mx-auto py-16 px-4 md:px-8 bg-transparent overflow-hidden">
       <div className="absolute top-0 right-0 h-screen w-full -z-20 opacity-25">
         <AnimatedBackground />
       </div>
