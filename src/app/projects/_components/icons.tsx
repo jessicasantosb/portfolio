655ab54a379b2102e735d55044e58118f18e42fd
Jessica <jessica.santosb@outlook.com>
2024-12-26 11:08:36 -0300

    feat: display stack name on mouse over

--- src/app/projects/_components/icons.tsx
+++ src/app/projects/_components/icons.tsx
@@ -1,18 +1,41 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 export function Icons({ stacks }: { stacks: string[] }) {
+  const [showName, setShowName] = useState(false);
+  const [currentStack, setCurrentStack] = useState("");
+
+  const handleMouseOver = (stackName: string) => {
+    setShowName(true);
+    setCurrentStack(stackName);
+  };
+
   return (
-    <div className="flex items-center flex-wrap justify-evenly gap-1 select-none">
+    <div className="relative flex items-center flex-wrap justify-evenly gap-1 select-none">
       {stacks.map((stack, index) => (
-        <div key={index} className="size-8 sm:size-12 relative">
-          <Image
-            alt="ícone"
-            src={`/assets/icons/${stack}.svg`}
-            fill
-            className="object-cover absolute"
-          />
+        <div key={index}>
+          <div
+            className="size-8 sm:size-12 relative"
+            onMouseOver={() => handleMouseOver(stack)}
+            onMouseLeave={() => setShowName(false)}
+          >
+            <Image
+              alt="ícone"
+              src={`/assets/icons/${stack}.svg`}
+              fill
+              className="object-cover absolute"
+            />
+          </div>
         </div>
       ))}
+
+      {showName && (
+        <div className="absolute -top-8 transition-shadow text-[#A888B5] text-lg">
+          {currentStack}
+        </div>
+      )}
     </div>
   );
 }
