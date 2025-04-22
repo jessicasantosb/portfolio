093e61b9f6e3dbe0a8a66b17ad4ab8ddf6a69bae
Jessica <jessica.santosb@outlook.com>
2025-04-22 09:20:18 -0300

    style: update hero width to large screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Hero } from "@/components/home/hero";
 
 export default function Home() {
   return (
-    <section className="max-w-6xl h-full mx-auto py-16 px-4 md:px-8 bg-transparent overflow-hidden">
+    <section className="max-w-9xl h-full mx-auto py-16 px-4 md:px-8 bg-transparent overflow-hidden">
       <div className="absolute top-0 right-0 h-screen w-full -z-20 opacity-25">
         <AnimatedBackground />
       </div>
